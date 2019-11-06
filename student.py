a6edcba2035c72e6296d7bca41bf1e6f0451aec2
Aidan Fuerpass <fuerpassa@gilmour.org>
2019-11-06 10:56:05 -0500

    added servo turn to midpoint

--- student.py
+++ student.py
@@ -176,6 +176,7 @@ class Piggy(PiggyParent):
         
         # TODO: build self.quick_check() that does a fast, 3-part check instead of read_distance
         while True:
+            self.servo(self.MIDPOINT)
             while self.read_distance() > 250:  # TODO: fix this magic number
                 self.fwd()
                 time.sleep(.01)

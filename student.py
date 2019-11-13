1aaf75e13afeac8c9d0a381710fe697a89e79f79
Aidan Fuerpass <fuerpassa@gilmour.org>
2019-11-13 14:54:49 -0500

    added quick check

--- student.py
+++ student.py
@@ -18,6 +18,7 @@ class Piggy(PiggyParent):
         '''
         self.LEFT_DEFAULT = 80
         self.RIGHT_DEFAULT = 80
+        self.SAFE_DIST = 350
         self.MIDPOINT = 1350  # what servo command (1000-2000) is straight forward for your bot?
         self.load_defaults()
         
@@ -163,6 +164,15 @@ class Piggy(PiggyParent):
         self.stop()
         print("I found this many things: %d" % count)
         return count 
+
+    def quick_check(self):
+        for ang in range(self.MIDPOINT-150, self.MIDPOINT+151, 150)
+        self.servo(ang)
+        if self.read_distance() < self.SAFE_DIST:
+            return False
+    
+    #if i return tru i didnt find anything
+    return True
 
            
         
